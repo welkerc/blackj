5007161b9dd1c5cd121160d0a317fb1f419d66b2
Christopher Welker <welker@chriswelker.net>
2014-07-27 23:09:16 -0700

    array test

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<html>
+<head>
+<title>sb</title>
+</head>
+<body>
+<p>
 <?php
+    $deckArr = array(2,3,4,5,6,7,8,9,10);
+    $suitArr = array('Hearts', 'Clubs', 'Dimonds', 'Spades');
+    $deck1 = $deckArr[rand(0, 8)];
+    $suit1 = $suitArr[rand(0, 3)];
+    $deck2 = $deckArr[rand(0, 8)];
+    $suit2 = $suitArr[rand(0, 3)];
+    $tp = $deck1 + $deck2;
+    
+    echo 'You have the ' . $deck1 . ' of ' . $suit1 . ' and ' . $deck2 . ' of ' . $suit2 . '.' . "<br /><br />";
+    
+    echo  'You have ' . $tp;ß
 
-?>
+    ?>
+</p>
+</body>
+</html>
