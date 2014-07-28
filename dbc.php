df1a02a040f63de6fd9c7c80852069d73846cfc3
Christopher Welker <welker@chriswelker.net>
2014-07-28 11:19:59 -0700

    update

--- dbc.php
+++ dbc.php
@@ -6,7 +6,18 @@
 <p>
 <?php   
    
-   
+    $mysqli = new mysqli("mysql.chriswelker.net", "cwelker", "minemine", "chriswelker_net");
+    
+    /* check connection */
+    #if ($mysqli->connect_errno) {
+    #   die("Connect failed: %s\n", $mysqli->connect_error);
+    #}
+    
+    $q = "SELECT * FROM blackj";
+    $r = $mysqli->query($q);
+    
+    $r = $result->fetch_array(mysqli_assoc);
+    printf ("%s ($s)\n", $row[0], $row["rank"]);
    
     ?>
 </p>
