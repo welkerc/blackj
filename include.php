df81170bf27d561374c8b951cfdc30263678451a
Christopher Welker <paradigms@gmail.com>
2014-10-01 16:46:57 -0700

    Update include.php

--- include.php
+++ include.php
@@ -1,9 +1,9 @@
 <?php
 
-$hn = "mysql.chriswelker.net";   // eg. mysql.yourdomain.com (unique)
-$un = "cwelker";   // the username specified when setting-up the database
-$pw = "minemine";   // the password specified when setting-up the database
-$dbn = "chriswelker_net";   // the database name chosen when setting-up the database (unique)
+$hn =    // eg. mysql.yourdomain.com (unique)
+$un =    // the username specified when setting-up the database
+$pw =    // the password specified when setting-up the database
+$dbn =   // the database name chosen when setting-up the database (unique)
 $sql = "SELECT * FROM `blackj` WHERE 1 LIMIT 0, 52 ";
 
 $db = mysql_connect($hn,$un,$pw);
